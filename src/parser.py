1ea982d01e3a2183e242a4fc2e3d540a504060dd
Rishabh Bhatnagar <bhatnagarrishabh4@gmail.com>
2023-01-20 17:38:13 +0530

    Fill Nix's Shell Processor
    Signed-off-by: Rishabh Bhatnagar <bhatnagarrishabh4@gmail.com>

--- src/parser.py
+++ src/parser.py
@@ -12,10 +12,12 @@ When we are given the config files,there are two types of statements:
 import abc
 import logging
 import os.path
+import pathlib
 
 import yaml
 
-from src.utils import Platform, get_platform, Constants
+from main import get_requirement
+from src.utils import Platform, get_platform, Constants, TextMode, format_text
 
 
 class Processor(abc.ABC):
@@ -32,6 +34,7 @@ class NixProcessor(Processor):
         Git config is a one time run thing. Once set, it will stay as-is with
         every new terminal instance
         """
+
         def _generate_git_stmts():
             for scope, section_vars in git_config.items():
                 for section, git_alias in section_vars.items():
@@ -45,7 +48,71 @@ class NixProcessor(Processor):
             os.system(stmt)
 
     def process_shell_config(self, shell_config, out_dir):
-        pass
+        def _generate_shell_stmt():
+            for abbr, cmd in all_aliases.items():
+                cmd = cmd.replace('"', '\\"')
+                yield f'alias {abbr}="{cmd}"'
+
+        def _add_file_to_rc(cmd):
+            def _detect_rc_file():
+                home_dir = pathlib.Path('~').expanduser()
+                rc_file_names = Constants.POSSIBLE_RC_FILENAMES
+                detected_path = None
+                for fname in rc_file_names:
+                    if (curr_rc_fname := home_dir.joinpath(fname)).exists():
+                        logging.debug("found a rc file: " + str(curr_rc_fname))
+                        detected_path = str(curr_rc_fname.absolute())
+                        break
+                if detected_path is None:
+                    return None
+
+                # confirm from user if the detected rc file path is correct
+                print(format_text([TextMode.BOLD], detected_path), 'is the detected path of rc file')
+                choice = get_requirement('confirmation_rc_path', 'y if you want to change the rc file path (any other key to continue)', False, False, out_dir)
+                if choice.lower() == 'y':
+                    return None
+                return detected_path
+
+            def _get_rc_file_path():
+                rc_file_path = _detect_rc_file()
+                if rc_file_path is None:
+                    rc_file_path = get_requirement(Constants.FIELD_NAME_rc_path,
+                                                   Constants.DESCRIPTION_rc_file,
+                                                   True, False,
+                                                   out_dir)
+                return os.path.realpath(os.path.expanduser(rc_file_path))
+
+            rc_file_path = _get_rc_file_path()
+            # create the rc file if it doesn't exist
+            pathlib.Path(rc_file_path).touch()
+
+            # return if the command is already in the rc file
+            with open(rc_file_path) as fh:
+                if cmd in fh.read().split('\n'):
+                    logging.debug(
+                        "Command already found in the current rc file")
+                    return
+
+            # cmd is not present in the rc file
+            logging.debug("The command was not found in the rc file. Adding it")
+            with open(rc_file_path, 'a') as fh:
+                fh.write(f'\n{cmd}\n')
+
+        general_alias = shell_config.get('alias', {})
+        nix_alias = shell_config.get('nix', {}).get('alias', {})
+        all_aliases = {**general_alias, **nix_alias}
+
+        out_file_path = os.path.join(out_dir, Constants.PATH_OUTPUT_main_config)
+        with open(out_file_path, 'w') as fh:
+            fh.write('\n'.join(_generate_shell_stmt()))
+        logging.info(
+            f'created a file {format_text([TextMode.BOLD], out_file_path)} that stores all the remaining config')
+
+        cmd = f'. {out_file_path}'
+        _add_file_to_rc(cmd)
+        logging.info("Output config file: " + format_text(
+            [TextMode.BOLD, TextMode.ITALIC, TextMode.UNDERLINE],
+            out_file_path))
 
 
 class WindowsProcessor(Processor):
@@ -62,13 +129,13 @@ class WindowsProcessor(Processor):
 
         general_aliases = shell_config.get('alias', {})
         win_specific_aliases = _get_win_specific_aliases(shell_config)
-        all_aliases = general_aliases
-        all_aliases.update(win_specific_aliases)
+        all_aliases = {**general_aliases, **win_specific_aliases}
         for key, cmd in all_aliases.items():
             with open(os.path.join(out_dir, key + '.bat'), 'w') as fh:
                 fh.write(cmd)
         set_path_cmd = f'setx path "%PATH%;{out_dir}"'
-        logging.debug('adding out_dir to cmd using following command: ' + set_path_cmd)
+        logging.debug(
+            'adding out_dir to cmd using following command: ' + set_path_cmd)
         os.system(set_path_cmd)
 
 
@@ -76,7 +143,9 @@ class ProcessorFactory:
     @staticmethod
     def get() -> Processor:
         if get_platform() == Platform.Windows:
+            logging.debug("detected a \033[1mWindows\033[0;m system")
             return WindowsProcessor()
+        logging.debug("detected a \033[1m*NIX\033[0;m system")
         return NixProcessor()
 
 
@@ -90,10 +159,12 @@ def parse_config_files(config_dir_path: str, out_dir: str):
                              config files will be stored.
     """
     processor = ProcessorFactory.get()
-    with open(os.path.join(config_dir_path, Constants.PATH_OUTPUT_shell_config_fname)) as fh:
+    with open(os.path.join(config_dir_path,
+                           Constants.PATH_OUTPUT_shell_config_fname)) as fh:
         shell_config = yaml.safe_load(fh.read())
 
-    with open(os.path.join(config_dir_path, Constants.PATH_OUTPUT_git_config_fname)) as fh:
+    with open(os.path.join(config_dir_path,
+                           Constants.PATH_OUTPUT_git_config_fname)) as fh:
         git_config = yaml.safe_load(fh.read())
 
     processor.process_git_config(git_config)
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,26 @@
 import enum
 import platform
+from functools import reduce
+from typing import Iterable
 
 
 class Constants:
+    POSSIBLE_RC_FILENAMES = ['.zshrc', '.bashrc']
+
     FIELD_NAME_required_vars = 'required_vars'
     FIELD_NAME_out_dir = 'out_dir'
     FIELD_NAME_platform_windows = 'windows'
+    FIELD_NAME_rc_path = 'rc_path'
 
     PATH_INPUT_config_dir_name = 'config_template'
     PATH_OUTPUT_config_dir_name = 'config'
     PATH_OUTPUT_git_config_fname = 'git.yaml'
     PATH_OUTPUT_shell_config_fname = 'shell.yaml'
     PATH_OUTPUT_cache_file_name = '.secrets'
+    PATH_OUTPUT_main_config = 'main.sh'
 
     DESCRIPTION_out_dir = 'directory path where all the config files will be stored'
+    DESCRIPTION_rc_file = 'path of the rc file (~/.bashrc for example)'
 
 
 class Platform(enum.Enum):
@@ -30,3 +37,19 @@ def get_platform():
         return Platform.Windows
     else:
         return Platform.Linux
+
+
+class TextMode(enum.Enum):
+    NORMAL = 0
+    BOLD = 1
+    ITALIC = 3
+    UNDERLINE = 4
+
+
+def _set_mode(text: str, mode: TextMode):
+    assert isinstance(mode, TextMode)
+    return f'\033[{mode.value}m{text}\033[0m'
+
+
+def format_text(modes: Iterable[TextMode], text: str) -> str:
+    return reduce(_set_mode, modes, text)
